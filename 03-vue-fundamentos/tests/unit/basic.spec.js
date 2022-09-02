899dc2b12d6199f9cfe49fe76a81e97a4ca304ba
hugoorlando <hugo.orlando2030@gmail.com>
2022-09-02 13:10:18 -0300

    se agrega componentes y test unitarios

---
+++ 03-vue-fundamentos/tests/unit/basic.spec.js
@@ -0,0 +1,13 @@
+import { describe, test, expect } from 'vitest';
+
+describe('Testing...', () => {
+  test('Debe de ser mayor a 10', () => {
+    // Arreglar
+    let value = 5;
+
+    // Estimulo
+    value += 10;
+    // Observar el resultado
+    expect(value).toBeGreaterThan(10);
+  });
+});
---
+++ 03-vue-fundamentos/tests/unit/components/indecision.spec.js
@@ -0,0 +1,26 @@
+import { describe, test, expect, beforeEach } from 'vitest';
+import { shallowMount } from '@vue/test-utils';
+
+import Indecision from '../../../src/components/Indecision.vue';
+
+describe('Indecision component', () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = shallowMount(Indecision);
+  });
+
+  test('debe de ser identico con el snapshot', () => {
+    // toMatchSnapshot() garantiza que un valor coincida con la instantánea más reciente.
+    expect(wrapper.html()).toMatchSnapshot();
+
+    // actualizar snapshot
+    // yarn test -u
+  });
+
+  test('p debe tener el valor por defecto "Recuerda terminar con un signo de interrogacion"', () => {
+    const parrafo = wrapper.find('p');
+    expect(parrafo.text()).toBe(
+      'Recuerda terminar con un signo de interrogacion'
+    );
+  });
+});
