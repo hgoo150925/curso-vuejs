be1d5c0e415315c7d609d90b247372d1882de14a
hugoorlando <hugo.orlando2030@gmail.com>
2022-09-02 13:05:02 -0300

    fundamentos app

---
+++ fundamentos-app/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <!-- <Counter :start="100" title="Hola Mundo" /> -->
+
+  <Indecision />
+</template>
+
+<script>
+// import Counter from './components/Counter.vue'
+import Indecision from './components/Indecision.vue';
+export default {
+  name: 'App',
+  components: {
+    // Counter,
+    Indecision,
+  },
+};
+</script>
